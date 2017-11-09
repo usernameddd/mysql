ed489149a4afaa4b6c09047305c8037e2a2338dc
usernameddd <dailinwork@163.com>
2017-11-09 09:16:24 +0800

    2

---
+++ download.sql
@@ -0,0 +1,38 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : my computer
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : download
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2017-11-09 08:56:10
+*/
+
+create database 'download';
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `goods`
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `price` float DEFAULT NULL,
+  `image` varchar(50) DEFAULT NULL,
+  `detail` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES ('1', '手机', null, null, null);
+INSERT INTO `goods` VALUES ('2', '电话', null, null, null);
+INSERT INTO `goods` VALUES ('3', '电脑', null, null, null);
